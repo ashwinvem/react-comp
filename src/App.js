887a7812c23fd9f1631437d1570bc1c073b48291
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-11-16 00:00:17 +0530

    NA

--- src/App.js
+++ src/App.js
@@ -2,9 +2,5 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <p>I am learning React. My life is getting better</p>
-    </div>
-  );
+  return <p>I am learning React. My life is getting better</p>;
 }
